cbe104dba8fedef0b57d130158bb242daf034d1f
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-09-22 12:00:05 -0400

    Variables

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,24 @@
 import PlaygroundSupport
 
-var greeting = "Hello, playground"
+
+
+//Long Form
+var name1 : String = "Alfonso"
+print(name1)
+
+//Short Form
+var name2 = "Andrews"
+print(name2)
+
+
+/* Optional form
+ ? - The data could or could not have a value,
+ not necessarily being null, but rather that i haven't
+ assigned a value to it yet.
+ */
+var name3 : String?
+name3 = "Alexander"
+print(name3)
+
+name1 = "Stephanie"
+print(name1)
